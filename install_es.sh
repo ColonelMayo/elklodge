c0d545deb38480f971bddbae17388508d6c0e67a
ColonelMayo <mac.blake.cole@gmail.com>
2016-12-21 10:26:25 -0600

    installation file updates

--- install_es.sh
+++ install_es.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
 #setup directory
-mkdir -p /usr/local/es &&
-cd /usr/local/es &&
-wget https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.7.5.tar.gz &&
-tar xvfz elasticsearch-1.7.5.tar.gz &&
+cd /usr/bin &&
+wget https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-5.1.1.tar.gz &&
+tar -xvfz elasticsearch-1.7.5.tar.gz &&
 
 #setup es in the global path
-echo "ES_HOME=/usr/local/es/elasticsearch-1.7.5" >> /etc/profile &&
+#echo "ES_HOME=/usr/local/es/elasticsearch-1.7.5" >> /etc/profile &&
 
-#setup the installation
-update-alternatives --install "/usr/bin/es" "es" "/usr/local/es/elasticsearch-1.7.5/bin/elasticsearch" 1 &&
-
-#setup a test command
-echo "alias estest=\"curl 'http://localhost:9200/?pretty'\"" >> ~/.bashrc &&
-source ~/.bashrc &&
-
-echo "run es with -d for daemon/background mode"
+echo "Script complete. You must manually edit /etc/profile to add ES_HOME to PATH. .bashrc to alias a test command. Read elastic.co documentation to run elasticsearch as a service."
--- install_ls.sh
+++ install_ls.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 #setup directory
-mkdir -p /usr/local/logstash &&
-cd /usr/local/logstash &&
-curl -O http://download.elastic.co/logstash/logstash/logstash-1.5.0.tar.gz &&
+
+cd /usr/bin
+wget https://artifacts.elastic.co/downloads/logstash/logstash-5.1.1.tar.gz &&
 tar xvfz logstash-1.5.0.tar.gz &&
 
 #setup ls in global path
-echo "LS_HOME=/usr/local/ls/logstash-1.5.0" >> /etc/profile &&
+#echo "LS_HOME=/usr/local/ls/logstash-1.5.0" >> /etc/profile &&
 
 #setup the installation
-update-alternatives --install "/usr/bin/logstash" "logstash" "/usr/local/logstash/logstash-1.5.0/bin/logstash" 1 &&      
 
-echo "Logstash setup is complete."
+echo "Script complete. To setup logstash...path variable...aliases...read manual for running as a service"
 
