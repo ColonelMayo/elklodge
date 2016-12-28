5a4be4fd8f878821a66b335a5c847ca275bb96fb
ColonelMayo <mac.blake.cole@gmail.com>
2016-12-28 10:43:26 -0600

    updated elasticsearch download link

--- install_es.sh
+++ install_es.sh
@@ -5,7 +5,8 @@ cd /usr/bin &&
 wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.1.1.tar.gz &&
 tar -xvz elasticsearch-5.1.1.tar.gz &&
 
-#setup es in the global path
-#echo "ES_HOME=/usr/local/es/elasticsearch-5.1.1" >> /etc/profile &&
-
 echo "Script complete. You must manually edit /etc/profile to add ES_HOME to PATH. .bashrc to alias a test command. Read elastic.co documentation to run elasticsearch as a service."
+
+#setup es in the global path
+#echo "ES_HOME=/usr/bin/elasticsearch-5.1.1" >> /etc/profile &&
+
