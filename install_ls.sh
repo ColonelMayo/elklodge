e3ad8e2bf4dff3ea7b522ab11a7b3e90867afb86
ColonelMayo <mac.blake.cole@gmail.com>
2016-12-29 14:57:14 -0600

    changing Logstash installation to debian package download

--- install_ls.sh
+++ install_ls.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 #setup directory
-
-cd /usr/bin
-wget https://artifacts.elastic.co/downloads/logstash/logstash-5.1.1.tar.gz &&
-tar xvfz logstash-5.1.1.tar.gz &&
+mkdir /logstash &&
+cd /logstash &&
+wget https://artifacts.elastic.co/downloads/logstash/logstash-5.1.1.deb &&
+dpkg -i logstash-5.1.1.deb &&
 
 #setup ls in global path
 #echo "LS_HOME=/usr/local/ls/logstash-1.5.0" >> /etc/profile &&
 
 #setup the installation
 
-echo "Script complete. To setup logstash...path variable...aliases...read manual for running as a service"
+#echo "Script complete. To setup logstash...path variable...aliases...read manual for running as a service"
 
