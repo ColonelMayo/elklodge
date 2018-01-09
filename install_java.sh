e8381d9ca1d0c36d62e92ff942146a815792d50e
ColonelMayo <mac.cole@dir.texas.gov>
2018-01-08 21:31:38 -0600

    Update install_java.sh

--- install_java.sh
+++ install_java.sh
@@ -8,7 +8,7 @@ cd /usr/local/java &&
 #download moved to http://download.oracle.com/otn-pub/java/jdk/8u151-b12/e758a0de34e24606bca991d704f6dcbf/jdk-8u151-linux-x64.tar.gz
 #moving forward with manual download and sftp of tar.gz file
 
-tar xvzf jdk-8u151-linux-x64.tar.gz &&
+tar xvzf jdk-8u151-linux-i586.tar.gz &&
 
 echo "JAVA_HOME=/usr/local/java/jdk1.8.0_151" >> /etc/profile &&
 echo "JRE_HOME=/usr/local/java/jre1.8.0_151/jre" >> /etc/profile &&
