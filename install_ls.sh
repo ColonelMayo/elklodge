e8291b72c98ee4762ae6ff3491969cf7f5635ec9
ColonelMayo <mac.cole@dir.texas.gov>
2018-01-08 21:25:28 -0600

    Update install_ls.sh

--- install_ls.sh
+++ install_ls.sh
@@ -3,5 +3,6 @@
 #setup directory
 mkdir /logstash &&
 cd /logstash &&
-wget https://artifacts.elastic.co/downloads/logstash/logstash-5.1.1.deb &&
-dpkg -i logstash-5.1.1.deb
+#wget https://artifacts.elastic.co/downloads/logstash/logstash-5.1.1.deb &&
+wget https://artifacts.elastic.co/downloads/logstash/logstash-6.1.1.deb &&
+dpkg -i logstash-6.1.1.deb
